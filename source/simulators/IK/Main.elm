d2e28fb898b466afcdbc532c5e50bda0e1ac4d14
Emil Segerbäck <emilsegerback@gmail.com>
2016-10-19 10:36:18 +0200

    Fix bugs in IK simulator

--- source/simulators/IK/Main.elm
+++ source/simulators/IK/Main.elm
@@ -23,27 +23,41 @@ import Html exposing (Html)
 import Html.App as App
 import Html.Attributes exposing (width, height)
 import WebGL
+import Math.Vector3 as Vector3 exposing (..)
 import Rendering
 import Mouse
+import AnimationFrame
+import Time exposing (Time)
 
 
 type alias Model =
-    { lastClick : Mouse.Position }
+    { lastClick : Vec3, animating : Bool, limbs : List ( Float, Float ), iterations : Int }
 
 
 type Msg
     = NoOp
     | MouseDown Mouse.Position
+    | Tick Time
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {lastClick = {x = 131, y = 32}}, Cmd.none )
+    ( { lastClick = vec3 131 32 0
+      , animating = False
+      , limbs =
+            [ ( 0, 50 ), ( pi / 4, 100 ), ( -2 * pi / 3, 40 ) ]
+      , iterations = 0
+      }
+    , Cmd.none
+    )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch [Mouse.downs MouseDown]
+    Sub.batch
+        [ Mouse.downs MouseDown
+        , AnimationFrame.diffs Tick
+        ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -53,13 +67,48 @@ update msg model =
             ( model, Cmd.none )
 
         MouseDown pos ->
-            ( {model | lastClick = pos}, Cmd.none )
+            let
+                offsetPos =
+                    (vec3 (toFloat pos.x - Rendering.halfCanvasWidth) -(toFloat pos.y - Rendering.halfCanvasHeight) 0)
+
+                _ =
+                    Debug.log "click at " offsetPos
+
+                _ =
+                    Debug.log "arm at " (Rendering.calcEndPos model.limbs)
+            in
+                ( { model | lastClick = offsetPos, animating = True, iterations = 0 }, Cmd.none )
+
+        Tick newTime ->
+            let
+                currentEnd =
+                    Rendering.calcEndPos model.limbs
+
+                toEnd =
+                    sub model.lastClick currentEnd
+            in
+                if model.animating && length toEnd > 10 then
+                    ( { model
+                        | limbs = Rendering.calculateIK model.lastClick model.limbs
+                        , iterations = model.iterations + 1
+                      }
+                    , Cmd.none
+                    )
+                else
+                    let
+                        _ =
+                            if model.animating then
+                                Debug.log "Found click position in " model.iterations
+                            else
+                                model.iterations
+                    in
+                        ( { model | animating = False }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     WebGL.toHtml [ width Rendering.canvasWidth, height Rendering.canvasHeight ]
-        (Rendering.renderAll model.lastClick)
+        (Rendering.renderAll model.limbs)
 
 
 main : Program Never
