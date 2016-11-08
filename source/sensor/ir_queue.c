7714c3773778ed82007d412a6d1f5bd271fbbef3
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-08 12:31:54 +0100

    Added gpl notices and implemented some more

--- source/sensor/ir_queue.c
+++ source/sensor/ir_queue.c
@@ -1,4 +1,25 @@
+// Copyright 2016 Noak Ringman, Emil Segerbäck, Robin Sliwa, Frans Skarman, Hannes Tuhkala, Malcolm Wigren, Olav Övrebö
+
+// This file is part of LiTHe Hex.
+
+// LiTHe Hex is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// LiTHe Hex is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with LiTHe Hex.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "ir_queue.h"
+
+///////////////////////////////////////////////////////////////////
+// "Private"
+///////////////////////////////////////////////////////////////////
 
 /*
  * Initializes an IRElem as a dummy.
@@ -15,6 +36,12 @@ bool is_dummy(IRElem* elem) {
     return elem->port == DUMMY_PORT;
 }
 
+bool contains(IRQueue* queue, irport_t port) {
+    // TODO implement
+}
+
+///////////////////////////////////////////////////////////////////
+
 void ir_queue_init(IRQueue* queue) {
 
     // set all elements to dummies
@@ -28,6 +55,6 @@ void ir_queue_init(IRQueue* queue) {
 }
 
 void schedule(IRQueue* queue, irport_t port) {
-
+    
 }
 
