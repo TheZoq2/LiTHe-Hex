070fb2ca3b87a5773bf80227398ad96a7f5ab406
Olav Övrebö <olav.ovrebo@telia.com>
2016-11-10 11:46:19 +0100

    Removed magic constants.

--- source/central_unit/pid_controller.py
+++ source/central_unit/pid_controller.py
@@ -4,6 +4,12 @@ import math
 input_file = "/tmp/hexsim/sensors"
 utput_file = "/tmp/hexsim/command"
 
+CORRIDOR_WIDTH = 0.8
+SENSOR_OFFSET = 0.1
+ANGLE_SCALEDOWN = 0.5
+MOVEMENT_SCALEDOWN = 0.1
+BASE_MOVEMENT = "0.1" #placeholder forward movement.
+
 def get_sensor_data():
     """
     Returns the fetched sensor data from the input file
@@ -33,16 +39,16 @@ def main():
         sensor_data = get_sensor_data()
 
         # offset for the robot length from mid in a corridor
-        offset = (0.8 * (sensor_data[2] + 0.1)/(sensor_data[1] + 0.2 + sensor_data[2])) - 0.4
+        offset = (CORRIDOR_WIDTH * (sensor_data[2] + SENSOR_OFFSET)/(sensor_data[1] + (2 * SENSOR_OFFSET) + sensor_data[2])) - (CORRIDOR_WIDTH/2)
 
-        command_y = (sensor_data[1] - sensor_data[2])/10
+        command_y = (sensor_data[1] - sensor_data[2]) * MOVEMENT_SCALEDOWN
 
         if abs(offset) > 0.1:
-            goal_angle = -sensor_data[0] - math.pi * offset / 2
+            goal_angle = -sensor_data[0] - math.pi * offset * ANGLE_SCALEDOWN
         else:
             goal_angle = -sensor_data[0]
 
-        command = "0.1," + str(command_y) + "," + str(goal_angle)
+        command = BASE_MOVEMENT + "," + str(command_y) + "," + str(goal_angle)
         write_utput_command(command)
 
 main()
