8c09edde31497f091d66be8923bd7139b4f814f1
Robin Sliwa <rorrosliwa@gmail.com>
2016-11-10 14:55:10 +0100

    wrote some on decision making and removed magic constants in pid controller

--- source/central_unit/pid_controller.py
+++ source/central_unit/pid_controller.py
@@ -4,6 +4,14 @@ import math
 input_file = "/tmp/hexsim/sensors"
 utput_file = "/tmp/hexsim/command"
 
+ANGLE = 0
+FRONT_RIGHT_SENSOR = 1
+BACK_RIGHT_SENSOR = 2
+FRONT_LEFT_SENSOR = 3
+BACK_LEFT_SENSOR = 4
+
+
+
 def get_sensor_data():
     """
     Returns the fetched sensor data from the input file
@@ -16,9 +24,11 @@ def get_sensor_data():
         split_content = file_content.split(",")
 
         return (
-            float(split_content[0]),
-            float(split_content[1]),
-            float(split_content[3])
+            float(split_content[ANGLE]),
+            float(split_content[FRONT_RIGHT_SENSOR]),
+            float(split_content[BACK_RIGHT_SENSOR]),
+            float(split_content[FRONT_LEFT_SENSOR]),
+            float(split_content[BACK_LEFT_SENSOR])
         )
 
 def write_utput_command(command):
@@ -33,14 +43,14 @@ def main():
         sensor_data = get_sensor_data()
 
         # offset for the robot length from mid in a corridor
-        offset = (0.8 * (sensor_data[2] + 0.1)/(sensor_data[1] + 0.2 + sensor_data[2])) - 0.4
+        offset = (0.8 * (sensor_data[FRONT_LEFT_SENSOR] + 0.1)/(sensor_data[FRONT_RIGHT_SENSOR] + 0.2 + sensor_data[FRONT_LEFT_SENSOR])) - 0.4
 
-        command_y = (sensor_data[1] - sensor_data[2])/10
+        command_y = (sensor_data[FRONT_RIGHT_SENSOR] - sensor_data[FRONT_LEFT_SENSOR])/10
 
         if abs(offset) > 0.1:
-            goal_angle = -sensor_data[0] - math.pi * offset / 2
+            goal_angle = -sensor_data[ANGLE] - math.pi * offset / 2
         else:
-            goal_angle = -sensor_data[0]
+            goal_angle = -sensor_data[ANGLE]
 
         command = "0.1," + str(command_y) + "," + str(goal_angle)
         write_utput_command(command)
