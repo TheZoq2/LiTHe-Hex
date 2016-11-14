8c1cd3501a9a121a6ca51260002e1a7b02912bb1
Frans Skarman <frans.skarman@gmail.com>
2016-11-14 14:31:26 +0100

    Add more than one leg to leg simulator

--- source/simulators/walk/src/main.rs
+++ source/simulators/walk/src/main.rs
@@ -36,9 +36,9 @@ impl Robot
     {
         let mut legs = vec!();
 
-        for i in 0..5
+        for i in 0..6
         {
-            legs.push(Leg::new(window, i as f32 * PI / 6.));
+            legs.push(Leg::new(window, i as f32 * PI / 3.));
         }
 
         Robot {
@@ -79,8 +79,6 @@ fn read_target_angles() -> Vec<Vec<f32>>
     //Reading the file into a string
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
-    println!("{:?}", file);
-    println!("{:?}", s);
 
     //Parsing the data
     let mut result = vec!();
@@ -101,7 +99,7 @@ fn read_target_angles() -> Vec<Vec<f32>>
     }
 
 
-    return result
+    result
 }
 
 fn main() {
@@ -120,11 +118,7 @@ fn main() {
 
         let target_angles = read_target_angles();
 
-        for angle in &target_angles
-        {
-            println!("{:?}", angle);
-        }
-
+        robot.set_target_angles(target_angles);
         robot.update(delta_time);
     }
 }
