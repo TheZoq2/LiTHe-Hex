2cb28cba1fda2685c8f7617873ba9220623e040c
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-15 10:34:13 +0100

    Implemented some of the lidar and i2c functions

--- source/sensor/lidar.c
+++ source/sensor/lidar.c
@@ -17,17 +17,36 @@
 
 #include "lidar.h"
 
-#define SENSOR_ADDR     0x32
+#define SENSOR_ADDR_READ     0xC4
+#define SENSOR_ADDR_WRITE    0xC5
+#define LIDAR_DATA_REG       0x8F
 
 void i2c_init(void);
 void i2c_send_start(void);
 void i2c_send_stop(void);
-void i2c_write_byte(uint8_t sens_addr, uint8_t reg, uint8_t data);
-uint8_t i2c_read_byte(uint8_t sens_addr, uint8_t reg);
+void i2c_write_byte(uint8_t reg, uint8_t data);
+void i2c_wait(void);
+uint8_t i2c_read_data(void);
 
 void lidar_add_value(Lidar* lidar, uint16_t centimeters);
 
 void lidar_init(Lidar* lidar) {
+    
+    i2c_init();
+
+    lidar->value = 0.0;
+
+}
+
+void lidar_measure(Lidar* lidar) {
+    i2c_write_byte(SENSOR_ADDR, 0x00, 0x04);
+    
+    i2c_wait();
+    
+    lidar_add_value(lidar, i2c_read_data());
+}
+
+void i2c_init(void) {
 
     // reset I2C status register
     TWSR = 0x00;
@@ -38,43 +57,110 @@ void lidar_init(Lidar* lidar) {
     // enable I2C
     TWCR = (1 << TWEN);
 
-
-}
-
-void lidar_measure(Lidar* lidar) {
-    i2c_write_byte(SENSOR_ADDR, 0x00, 0x04);
-    
-    while ((0xFE & i2c_read_byte(SENSOR_ADDR, 0x01)) != 0);
-
-    uint16_t least_sig = (uint16_t) i2c_read_byte(SENSOR_ADDR, 0x10);
-    uint16_t most_sig = (uint16_t) i2c_read_byte(SENSOR_ADDR, 0x0F);
-
-    uint16_t centimeters = (most_sig << 8) & least_sig;
-    
-    lidar_add_value(lidar, centimeters);
-}
-
-void i2c_init(void) {
-
 }
 
 void i2c_send_start(void) {
-
+    
 }
 
 void i2c_send_stop(void) {
 
 }
 
-void i2c_write_byte(uint8_t sens_addr, uint8_t reg, uint8_t data) {
+void i2c_write_byte(uint8_t reg, uint8_t data) {
+    i2c_send_start();
+    
+    // send sensor address
+    TWDR = SENSOR_ADDR_WRITE;
+    TWCR = (1 << TWINT)|(1 << TWEN);
+    while ((TWCR & (1 << TWINT)) == 0);
 
+    // send register address
+    TWDR = reg;
+    TWCR = (1 << TWINT)|(1 << TWEN);
+    while ((TWCR & (1 << TWINT)) == 0);
+
+    // send data 
+    TWDR = data;
+    TWCR = (1 << TWINT)|(1 << TWEN);
+    while ((TWCR & (1 << TWINT)) == 0);
+
+    i2c_send_stop();
 }
 
-uint8_t i2c_read_byte(uint8_t sens_addr, uint8_t reg) {
+uint16_t i2c_read_data(void) {
+    i2c_send_start();
+
+    // send sensor address
+    TWDR = SENSOR_ADDR_WRITE;
+    TWCR = (1 << TWINT)|(1 << TWEN);
+    while ((TWCR & (1 << TWINT)) == 0);
+
+    // send register address
+    TWDR = LIDAR_DATA_REG;
+    TWCR = (1 << TWINT)|(1 << TWEN);
+    while ((TWCR & (1 << TWINT)) == 0);
+
+    i2c_send_stop();
+
+    i2c_send_start();
+
+    // send sensor address, read
+    TWDR = SENSOR_ADDR_READ;
+    TWCR = (1 << TWINT)|(1 << TWEN);
+    while ((TWCR & (1 << TWINT)) == 0);
+    
+    TWCR = (1 << TWINT)|(1 << TWEN)|(1 << TWEA);
+    while ((TWCR & (1 << TWINT)) == 0);
+    uint8_t least_sig = TWDR;
+    
+    TWCR = (1 << TWINT)|(1 << TWEN)|(1 << TWEA);
+    while ((TWCR & (1 << TWINT)) == 0);
+    uint8_t most_sig = TWDR;
+
+    i2c_send_stop();
+    
+    return (most_sig << 8) & least_sig;
+}
+
+void i2c_wait() {
+    uint8_t status = 1;
+
+    while (status != 0) {
+        i2c_send_start();
+
+        // send sensor address
+        TWDR = SENSOR_ADDR_WRITE;
+        TWCR = (1 << TWINT)|(1 << TWEN);
+        while ((TWCR & (1 << TWINT)) == 0);
+
+        // send status register address
+        TWDR = 0;
+        TWCR = (1 << TWINT)|(1 << TWEN);
+        while ((TWCR & (1 << TWINT)) == 0);
+
+        i2c_send_stop();
+
+        i2c_send_start();
+
+        // send sensor address, read
+        TWDR = SENSOR_ADDR_READ;
+        TWCR = (1 << TWINT)|(1 << TWEN);
+        while ((TWCR & (1 << TWINT)) == 0);
+        
+        // read status register
+        TWCR = (1 << TWINT)|(1 << TWEN)|(1 << TWEA);
+        while ((TWCR & (1 << TWINT)) == 0);
+        
+        status = (0xFE & TWDR);
+
+        i2c_send_stop();
+        
+    }
 
 }
 
 void lidar_add_value(Lidar* lidar, uint16_t centimeters) {
-
+    
 }
 
