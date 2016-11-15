0522b9f664436ddc39e186ee4a16981b0afa109b
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-15 09:31:35 +0100

    Added some functions in lidar

--- source/sensor/lidar.c
+++ source/sensor/lidar.c
@@ -17,10 +17,40 @@
 
 #include "lidar.h"
 
+#define SENSOR_ADDR     0x32
+
+void i2c_init(void);
+void i2c_send_start(void);
+void i2c_send_stop(void);
+void i2c_write_byte(uint8_t sens_addr, uint8_t reg, uint8_t data);
+uint8_t i2c_read_byte(uint8_t sens_addr, uint8_t reg);
+
+void lidar_add_value(Lidar* lidar, uint16_t centimeters);
+
 void lidar_init(Lidar* lidar) {
-    // TODO implement
+
+    // reset I2C status register
+    TWSR = 0x00;
+
+    // set I2C bitrate to 400kHz
+    TWBR = 0x0C;
+
+    // enable I2C
+    TWCR = (1 << TWEN);
+
+
 }
 
 void lidar_measure(Lidar* lidar) {
-    // TODO implement
+    i2c_write_byte(SENSOR_ADDR, 0x00, 0x04);
+    
+    while ((0xFE & i2c_read_byte(SENSOR_ADDR, 0x01)) != 0);
+
+    uint16_t least_sig = (uint16_t) i2c_read_byte(SENSOR_ADDR, 0x10);
+    uint16_t most_sig = (uint16_t) i2c_read_byte(SENSOR_ADDR, 0x0F);
+
+    uint16_t centimeters = (most_sig << 8) & least_sig;
+    
+    lidar_add_value(lidar, centimeters);
 }
+
