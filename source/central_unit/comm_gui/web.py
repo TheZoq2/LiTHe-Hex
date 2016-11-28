514a9152e2b62bf0f7f9086348db24535c6eb6b0
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-28 13:32:18 +0000

    Added send test code

--- source/central_unit/comm_gui/web.py
+++ source/central_unit/comm_gui/web.py
@@ -1,5 +1,33 @@
-import pika
 import threading
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
+# connection = pika.BlockingConnection(pika.ConnectionParameters(
+#         host='localhost'))
+# channel = connection.channel()
+# 
+# channel.queue_declare(queue='hello')
+# 
+# def callback(ch, method, properties, body):
+#     print(" [x] Received %r" % body)
+# 
+# channel.basic_consume(callback,
+#                       queue='hello',
+#                       no_ack=True)
+# 
+# print(' [*] Waiting for messages. To exit press CTRL+C')
+# channel.start_consuming()
 
 # channel = None
 # 
@@ -21,37 +49,37 @@ import threading
 #     print(body)
 
 
-class CommunicationThread(threading.Thread):
-
-    def __init__(self, send_queue, receive_queue):
-        super().__init__()
-
-        self.channel = None
-
-        def handle_delivery(channel, method, header, body):
-            print(body)
-
-        def on_queue_declared(frame):
-            self.channel.basic_consume(handle_delivery, queue='test')
-
-        def on_channel_open(new_channel):
-            self.channel = new_channel
-            self.channel.queue_declare(queue="test", durable=True, exclusive=False,
-                                  auto_delete=False, callback=on_queue_declared)
-
-        def on_connected(connection):
-            self.connection.channel(on_channel_open)
-
-        self.send_queue = send_queue
-        self.receive_queue = receive_queue
-        self.parameters = pika.ConnectionParameters()
-        self.connection = pika.SelectConnection(self.parameters, on_connected)
-
-    def run(self):
-        self.connection.ioloop.start()
-
-    def stop(self):
-        self.connection.close()
+# class CommunicationThread(threading.Thread):
+# 
+#     def __init__(self, send_queue, receive_queue):
+#         super().__init__()
+# 
+#         self.channel = None
+# 
+#         def handle_delivery(channel, method, header, body):
+#             print(body)
+# 
+#         def on_queue_declared(frame):
+#             self.channel.basic_consume(handle_delivery, queue='hello')
+# 
+#         def on_channel_open(new_channel):
+#             self.channel = new_channel
+#             self.channel.queue_declare(queue="hello", durable=True, exclusive=False,
+#                                   auto_delete=False, callback=on_queue_declared)
+# 
+#         def on_connected(connection):
+#             self.connection.channel(on_channel_open)
+# 
+#         self.send_queue = send_queue
+#         self.receive_queue = receive_queue
+#         self.parameters = pika.ConnectionParameters()
+#         self.connection = pika.SelectConnection(self.parameters, on_connected)
+# 
+#     def run(self):
+#         self.connection.ioloop.start()
+# 
+#     def stop(self):
+#         self.connection.close()
 
 
 # parameters = pika.ConnectionParameters()
