5ee684993c2cb870549875c148a815f775ccf9c7
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-28 13:14:00 +0000

    Fixed errors

--- source/central_unit/comm_gui/web.py
+++ source/central_unit/comm_gui/web.py
@@ -1,29 +1,30 @@
 import pika
 import threading
 
-channel = None
-
-def on_connected(connection):
-    connection.channel()
-
-
-def on_channel_open(new_channel):
-    global channel
-    channel = new_channel
-    channel.queue_declare(queue="test", durable=True, exclusive=False, auto_delete=False, callback=on_queue_declared)
-
-
-def on_queue_declared(frame):
-    channel.basic_consume(handle_delivery, queue='test')
-
-
-def handle_delivery(channel, method, header, body):
-    print(body)
+# channel = None
+# 
+# def on_connected(connection):
+#     connection.channel()
+# 
+# 
+# def on_channel_open(new_channel):
+#     global channel
+#     channel = new_channel
+#     channel.queue_declare(queue="test", durable=True, exclusive=False, auto_delete=False, callback=on_queue_declared)
+# 
+# 
+# def on_queue_declared(frame):
+#     channel.basic_consume(handle_delivery, queue='test')
+# 
+# 
+# def handle_delivery(channel, method, header, body):
+#     print(body)
 
 
 class CommunicationThread(threading.Thread):
 
     def __init__(self, send_queue, receive_queue):
+        super().__init__()
 
         self.channel = None
 
@@ -31,7 +32,7 @@ class CommunicationThread(threading.Thread):
             print(body)
 
         def on_queue_declared(frame):
-            channel.basic_consume(handle_delivery, queue='test')
+            self.channel.basic_consume(handle_delivery, queue='test')
 
         def on_channel_open(new_channel):
             self.channel = new_channel
@@ -39,28 +40,28 @@ class CommunicationThread(threading.Thread):
                                   auto_delete=False, callback=on_queue_declared)
 
         def on_connected(connection):
-            connection.channel(on_channel_open)
+            self.connection.channel(on_channel_open)
 
         self.send_queue = send_queue
-        self.receive_queue = recieve_queue
+        self.receive_queue = receive_queue
         self.parameters = pika.ConnectionParameters()
         self.connection = pika.SelectConnection(self.parameters, on_connected)
 
     def run(self):
-        connection.ioloop.start()
+        self.connection.ioloop.start()
 
     def stop(self):
-        connection.close()
+        self.connection.close()
 
 
-parameters = pika.ConnectionParameters()
-connection = pika.SelectConnection(parameters, on_connected)
-
-try:
-    # Loop so we can communicate with RabbitMQ
-    connection.ioloop.start()
-except KeyboardInterrupt:
-    # Gracefully close the connection
-    connection.close()
+# parameters = pika.ConnectionParameters()
+# connection = pika.SelectConnection(parameters, on_connected)
+# 
+# try:
+#     # Loop so we can communicate with RabbitMQ
+#     connection.ioloop.start()
+# except KeyboardInterrupt:
+#     # Gracefully close the connection
+#     connection.close()
 
 
