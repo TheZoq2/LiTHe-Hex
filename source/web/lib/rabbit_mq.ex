dc4a43d725d490aa689c4eaca89f458001fb8334
Hannes Tuhkala <hantu447@student.liu.se>
2016-11-29 10:08:11 +0000

    communication from pi to gui works

--- source/web/lib/rabbit_mq.ex
+++ source/web/lib/rabbit_mq.ex
@@ -5,23 +5,27 @@ defmodule Web.RabbitMQ do
     {:ok, from_pi} = AMQP.Channel.open(connection)
     AMQP.Queue.declare(to_pi, "to_pi")
     AMQP.Queue.declare(from_pi, "from_pi")
-    AMQP.Basic.consume(from_pi, "from_pi", nil, no_ack: true)
-    spawn fn -> receive_message() end
+    Agent.start_link(fn -> %{connection: connection, to_pi: to_pi}  end, name: __MODULE__)
+    pid = spawn fn -> receive_message() end
+    AMQP.Basic.consume(from_pi, "from_pi", pid, no_ack: true)
   end
 
   def send_message(message) do
+    to_pi = Agent.get(__MODULE__, fn map -> map[:channel] end)
     AMQP.Basic.publish(to_pi, "", "to_pi", message)
   end
 
   def receive_message() do
     receive do
       {:basic_deliver, payload, _meta} ->
-        Web.Endpoint.broadcast("client", "new:msg", payload)
+        IO.puts payload
+        Web.Endpoint.broadcast("client", "new_msg", %{"body" => payload})
         receive_message
     end
   end
 
   def shutdown() do
+    connection = Agent.get(__MODULE__, fn map -> map[:connection] end)
     AMQP.Connection.close(connection)
   end
 end
