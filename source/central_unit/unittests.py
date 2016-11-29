de7969f18cb862718065db49745fcf79bf640403
Malcolm Wigren <trekommafem2@gmail.com>
2016-11-29 13:20:38 +0100

    Removed kravspec duplicates and communication test case

--- source/central_unit/unittests.py
+++ source/central_unit/unittests.py
@@ -8,7 +8,9 @@ import queue
 
 SENSOR_ARGS = (1, 2, 3, 4, 5, 0x01, 0xFE)
 CORRIDOR_ARGS = (0x01, 0xFE, 1, 2, 3, 180)
+CORRIDOR_ARGS_PARTIAL = (0x00, 0xEE, 1, 2, 3, 90)
 DEBUG_STRING_NORMAL = "hello"
+DEBUG_STRING_PARTIAL = "this is the debug string for the partial test"
 AUTO_NORMAL = False
 
 EXPECTED_NORMAL = {
@@ -27,6 +29,15 @@ EXPECTED_NORMAL = {
     "debug" : DEBUG_STRING_NORMAL
 }
 
+EXPECTED_PARTIAL = {
+    "dist_f" : 5.1,
+    "dist_d" : 0.01,
+    "dist_l" : 0.02,
+    "dist_r" : 0.03,
+    "corr_angle" : math.pi / 2,
+    "debug" : DEBUG_STRING_PARTIAL
+}
+
 class WebTestCase(unittest.TestCase):
 
     def test_send_packet_normal_json(self):
@@ -40,21 +51,18 @@ class WebTestCase(unittest.TestCase):
         json_string = send_packet.get_json()
         self.assertDictEqual(EXPECTED_NORMAL, json.loads(json_string))
 
+    def test_send_packet_partial(self):
+        corridor_packet = communication.CorridorDataPacket(*CORRIDOR_ARGS_PARTIAL)
+        send_packet = web.ServerSendPacket(
+            corridot_data_packet=corridor_packet,
+            debug=DEBUG_STRING_PARTIAL)
+        json_string = send_packet.get_json()
+        self.assertDictEqual(EXPECTED_NORMAL, json.loads(json_string))
+
+
     def test_send_packet_empty(self):
         send_packet = web.ServerSendPacket()
         json_string = send_packet.get_json()
         self.assertDictEqual({}, json.loads(json_string))
-
-
-class CommunicationTestCase(unittest.TestCase):
-    
-    def test_sensor_data_packet(self):
-        pass
-
-    def test_corridor_data_packet(self):
-        pass
-
-    def test_add_parity(self):
-        pass
 
 
