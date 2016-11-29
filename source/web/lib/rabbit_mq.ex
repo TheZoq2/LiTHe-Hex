4a9eb5dc706e7432b05a4744be027685809550d4
Hannes Tuhkala <hantu447@student.liu.se>
2016-11-29 08:53:36 +0000

    Added receive/send rabbitmq module

--- source/web/lib/rabbit_mq.ex
+++ source/web/lib/rabbit_mq.ex
@@ -1,9 +1,27 @@
-defmodule Web.PubSub.RabbitMQ do
-  def start_link(name, options) do
-    Supervisor.start_link(__MODULE__, {name, options},
-      name: Module.concat(name, Supervisor))
+defmodule Web.RabbitMQ do
+  def init() do
+    {:ok, connection} = AMQP.Connection.open
+    {:ok, to_pi} = AMQP.Channel.open(connection)
+    {:ok, from_pi} = AMQP.Channel.open(connection)
+    AMQP.Queue.declare(to_pi, "to_pi")
+    AMQP.Queue.declare(from_pi, "from_pi")
+    AMQP.Basic.consume(from_pi, "from_pi", nil, no_ack: true)
+    spawn fn -> receive_message() end
   end
 
-  def init({name, options}) do
+  def send_message(message) do
+    AMQP.Basic.publish(to_pi, "", "to_pi", message)
+  end
+
+  def receive_message() do
+    receive do
+      {:basic_deliver, payload, _meta} ->
+        Web.Endpoint.broadcast("client", "new:msg", payload)
+        receive_message
+    end
+  end
+
+  def shutdown() do
+    AMQP.Connection.close(connection)
   end
 end
--- source/web/web/static/js/socket.js
+++ source/web/web/static/js/socket.js
@@ -6,7 +6,7 @@
 import {Socket} from "phoenix"
 
 let socket = new Socket("/socket", {
-	params: {token: window.userToken}
+	params: {token: window.userToken},
 	logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
 })
 
