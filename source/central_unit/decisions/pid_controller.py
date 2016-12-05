6f6074f4c188cde9e4f59c18ed5dd058c60625cd
Robin Sliwa <rorrosliwa@gmail.com>
2016-12-05 17:38:45 +0100

    added more text to decision making in tekdok

--- source/central_unit/decisions/pid_controller.py
+++ source/central_unit/decisions/pid_controller.py
@@ -25,14 +25,14 @@ def _to_radians(deg):
 
 
 def regulate(sensor_data, decision_packet):
-    
+
     # offset for the robot length from mid in a corridor
     offset = (CORRIDOR_WIDTH * (sensor_data.ir_front_left + SENSOR_OFFSET)/(sensor_data.ir_front_right + (2 * SENSOR_OFFSET) + sensor_data.ir_front_left)) - (CORRIDOR_WIDTH/2)
-    
+
     decision_packet.command_y = (sensor_data.ir_front_right - sensor_data.ir_front_left) * decision_packet.movement_scaledown
     # TODO: fix a check for if one angle is way off. If it happens do not use it
     # TODO: fix so that we do not use positive angle all the time
-    
+
     if abs(offset) > decision_packet.angular_adjustment_border:
         decision_packet.regulate_goal_angle = -_to_radians(sensor_data.average_angle) - math.pi * offset * decision_packet.angle_scaledown
     else:
