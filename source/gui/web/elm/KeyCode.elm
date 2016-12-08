5387b64683f0805db75c2678ef5af2a27cc9e84a
Emil Segerbäck <emilsegerback@gmail.com>
2016-12-08 18:00:35 +0100

    Add sliders to control bot without joystick

---
+++ source/gui/web/elm/KeyCode.elm
@@ -0,0 +1,74 @@
+module KeyCode exposing (..)
+
+import Keyboard
+
+
+w : Keyboard.KeyCode
+w =
+    87
+
+
+a : Keyboard.KeyCode
+a =
+    65
+
+
+s : Keyboard.KeyCode
+s =
+    83
+
+
+d : Keyboard.KeyCode
+d =
+    68
+
+
+q : Keyboard.KeyCode
+q =
+    81
+
+
+e : Keyboard.KeyCode
+e =
+    69
+
+
+r : Keyboard.KeyCode
+r =
+    82
+
+
+c : Keyboard.KeyCode
+c =
+    67
+
+
+-- update : Bool -> KeyboardData -> Keyboard.KeyCode -> KeyboardData
+-- update down keys keyCode =
+--     case kecCode of
+--         87 ->
+--             { keys | w = down }
+
+--         65 ->
+--             { keys | a = down }
+
+--         83 ->
+--             { keys | s = down }
+
+--         68 ->
+--             { keys | d = down }
+
+--         81 ->
+--             { keys | q = down }
+
+--         69 ->
+--             { keys | e = down }
+
+--         82 ->
+--             { keys | r = down }
+
+--         67 ->
+--             { keys | c = down }
+
+--         _ ->
+--             keys
