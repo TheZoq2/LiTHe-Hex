ea61677649871f0c56fb06343c174f7965977b41
Hannes Tuhkala <hannesmt@hotmail.com>
2016-12-12 19:48:30 +0100

    Removed ik folder, ik is in motorik now

--- source/IK/for_debug.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "ik.h"
-
-int main() {
-    struct Leg leg = leg_ik(30, 0, 30);
-    printf("angle1 = %f\n", leg.angle1);
-    printf("angle2 = %f\n", leg.angle2);
-    printf("angle3 = %f\n", leg.angle3);
-}
